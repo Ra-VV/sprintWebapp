e15c54ea640bd6e21a35dff6572aeb56e1f18732
Rafal Goralski <Rafal_Goralski@epam.com>
2022-07-06 12:52:32 +0200

    Initial commit

---
+++ sprintWebapp/src/main/java/test/springframework/sprintWebapp/SprintWebappApplication.java
@@ -0,0 +1,13 @@
+package test.springframework.sprintWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintWebappApplication.class, args);
+	}
+
+}
---
+++ sprintWebapp/src/test/java/test/springframework/sprintWebapp/SprintWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package test.springframework.sprintWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
